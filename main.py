6c4aba99f44bfb4a27c25ca049af57e9af49b3a2
Not so Pro Python Tutorials YT <83178990+agy2021@users.noreply.github.com>
2022-01-14 16:52:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from pygame import mixer
 
+mixer.init()
+
+music = st.file_uploader("Choose a song.")
+
+try:
+    mixer.music.load(music)
+except Exception:
+    st.write("Please choose a song.")
+
+if st.button("Play"):
+    mixer.music.play()
+
+if st.button("Stop"):
+    mixer.music.stop()
+
+if st.button("Resume"):
+    mixer.music.unpause()
+
+if st.button("Pause"):
+    mixer.music.pause()
